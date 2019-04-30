ca0d25a7132d2b1d807e3d7f0cee9e56d6719f02
olewhalehunter <andersenpuckett@gmail.com>
2019-04-30 16:37:16 +0000

    writing

---
+++ book.el
@@ -0,0 +1,66 @@
+
+Automata Algebra for a Neurobiomechanics and Cognition
+"ALGAE" means something like "some algebraic-geometric automata environ stuff"
+
+;; Table of Contents
+(see these contents)
+
+;; Introduction
+
+(Stalin, Nikolai Bernstein, the Internet and SAGE, Vannevar Bush, labchan)
+(public works projects and productivity of humanity)
+(style of book, Common Lisp Elements, Steve Vogel DIY books biomechanics)
+(explain ALGAE as go, tutorial back book)
+
+;; 1. Signs and Connections
+
+There is a supposed divide between "symbolic" and "neural" methods in artificial intelligence, the former usually referring to discrete structure grammars or automata, and the latter referring to statistical or vector models.
+
+It surfaced as, when the field of "AI" expanded in the 80s, experts in signal processing found themselves working more in statistical connectionist programs and engineers in circuit design and mathematics found themselves working using symbolic methods for theorem proving or planning, among other things.
+
+To debunk this notion I will refer to the founding literature on neural networks and state machines.
+
+In McCulloch & Pitts (1943) [1], cognitive function is modeled as a State Machine, the authors inspiration partially inspired from logical symbolic analyses of syntax in language.
+
+
+;; (ALGAE) Shark and Jet gang map example
+
+Rumelhart & co (1986) [2] demonstrate as a first graphical example in the founding book the first parallel distributed representation of knowledge processes as a symbolic map , in the "Sharks and Jets" gang map. Early expert systems also included "semantic maps" with predicate labels for relations, which later became "fuzzy cognitive maps".
+
+;; 2. Parallel Distributed Processing [2]
+
+It is noteworthy that the revival of neural connectionism in the 80s, by Rumelhart & Co, were primarily cognitive scientists, not programmers or electrical engineers, looking for a mechanism of operation of the nervous system, and found working formalisms to some degree of success.
+
+The foundations of their work in embodied systems mirror earlier biomechanics research of Nikolai Bernstein [3] in constrain satisfaction for task completion. For example, a distributed coordination of the joints in an arm must be made so it can grab a cap through refuse on a desk top (demonstrated in [2] on page ...)
+
+;; (ALGAE) Bernstein joint constraint example
+
+
+So working from earlier electromechanical work and information theoretic work in neurons and perceptrons (see Minsky), they suggest a distributed neural model for coordination and knowledge representative schemas.
+
+
+;; 3. Automata Algebra
+
+'()
+
+Discrete vs Continuous
+
+;; (ALGAE) Automata Algebra reasoning and simulation
+;; exercises for reasoning about system with automat algebr & ALGAE
+
+
+;; 4. Cognitive Language and Affordance
+
+Vision and Action, James J Gibson and ecological perception. Conditioning Affordance. Human tool use, gesture, thought and affordance, Gourhain.
+
+
+;; 5. Schools of Interoception
+
+Yoga, buddhist meditation, christian meditation, biofeedback, science
+
+;; References and ALGAE tutorial
+
+
+[1] McCulloch & Pitts "A Logical Calculus of the Ideas Immanent in Nervous Activity", (1943)
+[2] Rumelhart & McClelland "Parallel Distributed Processing" Vol. I, (1986)
+[3] Bernstein, Nikolai (1967). "The Coordination and Regulation of Movements."
